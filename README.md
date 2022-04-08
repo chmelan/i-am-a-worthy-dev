8c06da5bfcb056411a7526a9d94125f1ec4ce73b
Nick Chmela <nickchmela@gmail.com>
2022-04-08 11:48:51 -0400

    configured proxy server

---
+++ README.md
@@ -0,0 +1,17 @@
+### Dev start: 
+
+**Terminal 1**
+```
+cd my-app
+npm run start:dev
+```
+
+**Terminal 2**
+```
+cd api
+npm run dev
+```
+
+Proxy server based on this setup:
+
+https://medium.com/bb-tutorials-and-thoughts/how-to-develop-and-build-next-js-app-with-nodejs-backend-7ff91841bd3
---
+++ client/components/Profile.tsx
@@ -0,0 +1,24 @@
+import useSWR from "swr";
+
+const fetcher = (url: string) =>
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+
+const Profile = () => {
+  const { data, error } = useSWR("/api/v1/users", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  console.table(data);
+  return (
+    <div>
+      <h1>IT WORKS!</h1>
+    </div>
+  );
+};
+
+export default Profile;
