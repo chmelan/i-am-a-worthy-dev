4eebc8f0c40e054ef73d18fc26849253ae9e9f6a
Nick Chmela <nickchmela@gmail.com>
2022-05-23 08:43:40 -0400

    Cleaned out repository

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
 import type { ReactElement } from 'react';
 import Layout from '../components/Layout';
-import UploadForm from '../components/UploadForm';
-import { useRouter } from 'next/router';
-import { decodeURL } from '../lib/serializer';
 const Home = () => {
-  //initialize the router hook
-  const router = useRouter();
-
-  //get data from URL if it exists
-  const list: any = router.query.list
-    ? decodeURL(router.query.list)
-    : undefined;
-
   return (
     <main>
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
@@ -19,7 +8,7 @@ const Home = () => {
 
         <div className="px-4 py-8 sm:px-0">
           <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
-            {list ? <div>List Uploaded!</div> : <UploadForm />}
+            Content 💗
           </div>
         </div>
         {/* /End replace */}
