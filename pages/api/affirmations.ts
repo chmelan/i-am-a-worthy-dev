9cc905d0392395401d484a0d0d9d2d393c931347
chmelan <nickchmela@gmail.com>
2022-06-08 11:27:00 -0400

    added error handling to main API call

--- pages/api/affirmations.ts
+++ pages/api/affirmations.ts
@@ -3,7 +3,16 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../lib/mongodb';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
   const db = await (await clientPromise).db('myFirstDatabase');
   const affirmation = await db.collection('affirmations').findOne();
-  res.status(200).json(affirmation.content);
+
+
+  if (affirmation) {
+    res.status(200).json(affirmation.content);
+  }
+  else {
+    res.status(404).send("Sorry, we couldn't find an affirmation for you")
+  }
+
 };
